94bcd3810afc002ced15746fb6a4d2d4bf48b155
neeraj <me.neeraj1455@gmail.com>
2022-09-02 14:50:31 +0530

    complete blog app assignment

--- block-BNaadm/blog/models/comment.js
+++ block-BNaadm/blog/models/comment.js
@@ -5,10 +5,12 @@ let Schema = mongoose.Schema;
 
 let commentSchema = new Schema(
   {
-    content: { type: String },
+    content: { type: String, required: true },
     author: { type: String },
-    articleId: { type: Schema.Types.ObjectId, ref: `Article` },
+    articleId: { type: Schema.Types.ObjectId, ref: `Article`, required: true },
+    likes: { type: Number, default: 0 },
   },
+
   { timestamps: true }
 );
 
